241a6a09b6c9de20c7d1a3b4b932a8f31e2feae5
SamyCode <sambea2103@gmail.com>
2023-08-07 18:43:29 -0400

    Routes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,46 @@
-import { useEffect, useState } from 'react'
 import HomePage from './pages/HomePage'
 import DiscoverPage from './pages/DiscoverPage'
 import FavoritesPage from './pages/FavoritesPage'
 import DetailPage from './pages/DetailPage'
 import AboutPage from './pages/AboutPage'
+import Page404 from './pages/Page404'
+
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Router from './components/Router'
+
 import './App.css'
-import { EVENTS } from './constants/const'
+
 
 function App() {
-  const [currentPath, setCurrentPath] = useState(window.location.pathname)
   
-  useEffect(()=>{
-    const onLocationChange = () => {
-      setCurrentPath(window.location.pathname)
+  const routes = [
+    {
+      path:'/',
+      Component: HomePage
+    },
+    {
+      path: '/about',
+      Component: AboutPage
+    },
+    {
+      path:'/discover',
+      Component: DiscoverPage
+    },
+    {
+      path: '/favorites',
+      Component: FavoritesPage
+    },
+    {
+      path:'/discover/:movie/',
+      Component: DetailPage
     }
+  ]
 
-    window.addEventListener(EVENTS.PUSHSTATE, onLocationChange)
-    window.addEventListener(EVENTS.POPSTATE, onLocationChange)
-    
-    return () => {
-      window.removeEventListener(EVENTS.PUSHSTATE, onLocationChange)
-      window.addEventListener(EVENTS.POPSTATE, onLocationChange)
-    }
-
-  }, [])
   return (
     <>
       <Header />
-      {currentPath === '/' && <HomePage />}
-      {currentPath === '/about' && <AboutPage />}
-      {currentPath === '/discover' && <DiscoverPage />}
-      {currentPath === '/favorites' && <FavoritesPage />}
-      {currentPath === '/detail' && <DetailPage />}
+        <Router routes={routes} defaultComponent={Page404} />
       <Footer />
     </>
   )
--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,7 +1,26 @@
-function Router({routes, defaultComponent }) {
-  return (
-    <div>Router</div>
-  )
+import {useEffect, useState } from 'react'
+import { EVENTS } from '../constants/const'
+
+function Router({routes=[], defaultComponent: DefaultComponent = () => <h1>404</h1> }) {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname)
+  
+  useEffect(()=>{
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname)
+    }
+
+    window.addEventListener(EVENTS.PUSHSTATE, onLocationChange)
+    window.addEventListener(EVENTS.POPSTATE, onLocationChange)
+    
+    return () => {
+      window.removeEventListener(EVENTS.PUSHSTATE, onLocationChange)
+      window.addEventListener(EVENTS.POPSTATE, onLocationChange)
+    }
+
+  }, [])
+
+  const Page = routes.find(({path})=> path === currentPath)?.Component
+  return Page ? <Page /> : <DefaultComponent />
 }
 
 export default Router
